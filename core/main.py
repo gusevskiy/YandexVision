0e8b745fd3d62b2e9efc0675061012e0128680e6
пгыуgusevskiy <vlgu@mail.ru>
2024-04-07 23:12:23 +0300

    сквозной из main работает

--- core/main.py
+++ core/main.py
@@ -1,44 +1,69 @@
-import os, glob
+# Импортируйте библиотеку для кодирования в Base64
+import base64
+import os
 import json
-from datetime import datetime, timedelta
-import response_IAM_token as response_IAM_token
+import requests
 from settings import settings
+from response_IAM_token import check_iam_token
 
 
-def iam_token(path_file_iamtoken):
-    # По умолчанию считаем что обновление требуется
-    token_update = False
-    for file in os.listdir(path_file_iamtoken):
-        if file.startswith("iam_token"):
-            # открывает json file iam_token запрошенный ранее.
-            with open(f"{path_file_iamtoken}{file}", "r") as file:
-                iam_token = json.load(file)
-            # дата и время окончания токена
-            expiresAt = iam_token["expiresAt"]
-            # переводим в формат "2024-04-07 03:55:09.855960"
-            token_run_out_this_time = datetime.strptime(
-                expiresAt[:26]+'Z', '%Y-%m-%dT%H:%M:%S.%fZ'
-            )
-            # если до окончания токена осталось меньше 2х часов.
-            if token_run_out_this_time - datetime.today() < timedelta(hours=2):
-                token_update = response_IAM_token.response_iam_token()
-                break
+def get_iam_token(iam_token_dir):
+    if check_iam_token(iam_token_dir):
+        file_iam_token = f"{iam_token_dir}\{os.listdir(iam_token_dir)[0]}"
+        with open(f"{file_iam_token}", "r", encoding="utf8") as file:
+                iam_token = json.load(file)["iamToken"]
+        # print(file_iam_token)
+        # # iam_token = file_iam_token["iamToken"]
+        return iam_token
+
+
+
+
+# Создайте функцию, которая кодирует файл и возвращает результат.
+def create_image_in_base64(input_image_file):
+    with open(input_image_file, "rb") as f:
+        file_content = f.read()
+        encoded_content = base64.b64encode(file_content).decode("utf-8")
+    # return encoded_content
+    data = {
+        "mimeType": "JPEG",
+        "languageCodes": ["*"],
+        "model": "passport",
+        "content": encoded_content
+    }
+    return json.dumps(data, indent=4)
+
+
+def main(image_path, iam_token_dir):
+    iam_token = get_iam_token(iam_token_dir)
+    json_data = create_image_in_base64(image_path)
+
+    # with open("output.json", "w") as json_file:
+    #     json_file.write(json_data)
+
+    url = "https://ocr.api.cloud.yandex.net/ocr/v1/recognizeText"
+
+    headers = {
+        "Authorization": f"Bearer {iam_token}",
+        "Content-Type": "application/json",
+        "x-folder-id": settings.privacy.x_folder_id,
+        "x-data-logging-enabled": "true"
+    }
+
+    response = requests.post(url, data=json_data, headers=headers)
+
+    if response.status_code == 200:
+         data_json = response.json()
+         file_name = "C:\\DEV_python\\YandexVision\\result_files_json\\file.json"
+         with open(file_name, "w", encoding="utf-8") as json_file:
+            json.dump(data_json, json_file, ensure_ascii=False, indent=4)
+        # print(response.json())
     else:
-        token_update = response_IAM_token.response_iam_token()
-    return token_update
-
-
-def main():
-    
-    iam_token()
-    ...
-
-
-
+        print("Ошибка", response.status_code)
+        
 
 if __name__ == '__main__':
-    path_file_iamtoken = (os.path.join(os.getcwd(), "iam_token\\"))
-    # print(path_file_iamtoken)
-    # print(os.path.isfile(path_file_iamtoken))
-    print(iam_token(path_file_iamtoken))
-    # print(path_file_iamtoken)
+    iam_token_dir = os.path.join(os.getcwd(), "iam_token")
+    image_path = "C:\\DEV_python\\YandexVision\\input_image\\1.jpg"
+    main(image_path, iam_token_dir)
+    # main(input_image_file)
--- core/response_IAM_token.py
+++ core/response_IAM_token.py
@@ -1,7 +1,7 @@
 import requests
 import os, glob 
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 from log_config import setup_logger
 from loguru import logger
 from settings import settings
@@ -11,12 +11,12 @@ from settings import settings
 setup_logger()
 
 
-def delete_file_iam_token():
+def delete_file_iam_token(iam_token_dir):
     """
     Удаляет файл iam_token*.json
     """
     try:
-        file_iam_token = glob.glob(os.getcwd()+'/iam_token*')[0]
+        file_iam_token = f"{iam_token_dir}\{os.listdir(iam_token_dir)[0]}"
         if file_iam_token:
             os.remove(file_iam_token)
             logger.info(f'Deleted old file: file_iam_token {file_iam_token}')
@@ -26,14 +26,14 @@ def delete_file_iam_token():
         logger.error(f'Error deleting file_iam_token: {e}')
 
 # 
-def response_iam_token():
+def response_iam_token(iam_token_dir) -> bool:
     """
     Запрашивает iam_token для Authorization в HTTP запросе.
+    Сохраняет ответ в файл iam_token_date.json.
     """
     try:
         # сначало удаляем файл если он есть
-        delete_file_iam_token()
-
+        delete_file_iam_token(iam_token_dir)
         # запрашиваем IAM_token
         iam_token_response = requests.post(
             'https://iam.api.cloud.yandex.net/iam/v1/tokens',
@@ -45,7 +45,7 @@ def response_iam_token():
         iam_token = iam_token_response.json()
         # сохраняем файл iam_token_текущая_дата.json
         file_name = f"iam_token {datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.json"
-        with open(file_name, "w") as json_file:
+        with open(f"{iam_token_dir}\{file_name}", "w") as json_file:
             json.dump(iam_token, json_file, indent=4)
             logger.info(f"Created new file: {file_name}")
         return True
@@ -57,6 +57,47 @@ def response_iam_token():
     return False
 
 
+def check_iam_token(iam_token_dir) -> bool:
+    """
+    Ф-я Проверяет существующий iam_token.json метод expiresAt
+    на предмент окнчания действия токена.
+    Если до окончания жизни токена менее 2х часов,
+    звпрашивается новый.
+    Если все запросы прошли ф-я возвращает True
+    """
+    # По умолчанию считаем что обновление требуется
+    token_update = False
+    for file in os.listdir(iam_token_dir):
+        if file.startswith("iam_token"):
+            # открывает json file iam_token котрый в папке iam_token.
+            with open(f"{iam_token_dir}\{file}", "r") as file:
+                iam_token = json.load(file)
+            # дата и время окончания токена
+            expiresAt = iam_token["expiresAt"]
+            # переводим в формат "2024-04-07 03:55:09.855960"
+            token_run_out_this_time = datetime.strptime(
+                expiresAt[:26]+'Z', '%Y-%m-%dT%H:%M:%S.%fZ'
+            )
+            # если до окончания токена осталось меньше 2х часов, запрашиваем новый.
+            if token_run_out_this_time - datetime.today() < timedelta(hours=2):
+                token_update = response_iam_token(iam_token_dir)
+                logger.info(f"Update token {file}")
+                break
+            else:
+            # если время еще позволяет используем существующий токен
+                token_update = True
+                logger.info("token is valid")
+                break
+    # Если файла с токеном ввобще нет, запрашиваем новый токен.
+    else:
+        token_update = response_iam_token(iam_token_dir)
+        logger.warning("File iam_token.json не был обнаружен!!! Создали новый.")
+    return token_update
+
+
 if __name__ == '__main__':
-    response_iam_token()
+    iam_token_dir = os.path.join(os.getcwd(), "iam_token")
+    # print(f"{iam_token_dir}\{os.listdir(iam_token_dir)[0]}")
+    print(check_iam_token(iam_token_dir))
     # delete_file_iam_token()
+    # print(os.listdir(os.path.join(os.getcwd(), "iam_token\\"))[0]) 
