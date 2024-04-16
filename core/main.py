46961d0d5c40e45c0851a0aba2c69f4bccce53d4
gusevskiy <vlgu@mail.ru>
2024-04-16 11:44:15 +0300

    НУжно переделать полностью логику, чтоб папки создовались сами и добавлялся ключ в .env файл и уже от туда его проверять

--- core/main.py
+++ core/main.py
@@ -9,25 +9,19 @@ from response_IAM_token import check_iam_token
 
 def get_iam_token(iam_token_dir):
     if check_iam_token(iam_token_dir):
-        file_iam_token = f"{iam_token_dir}\{os.listdir(iam_token_dir)[0]}"
+        file_iam_token = f"{iam_token_dir}\\{os.listdir(iam_token_dir)[0]}"
         with open(f"{file_iam_token}", "r", encoding="utf8") as file:
                 iam_token = json.load(file)["iamToken"]
-        # print(file_iam_token)
-        # # iam_token = file_iam_token["iamToken"]
         return iam_token
 
 
-
-
-# Создайте функцию, которая кодирует файл и возвращает результат.
 def create_image_in_base64(input_image_file):
     with open(input_image_file, "rb") as f:
         file_content = f.read()
         encoded_content = base64.b64encode(file_content).decode("utf-8")
-    # return encoded_content
     data = {
         "mimeType": "JPEG",
-        "languageCodes": ["*"],
+        "languageCodes": ["ru"],
         "model": "passport",
         "content": encoded_content
     }
@@ -37,9 +31,6 @@ def create_image_in_base64(input_image_file):
 def main(image_path, iam_token_dir):
     iam_token = get_iam_token(iam_token_dir)
     json_data = create_image_in_base64(image_path)
-
-    # with open("output.json", "w") as json_file:
-    #     json_file.write(json_data)
 
     url = "https://ocr.api.cloud.yandex.net/ocr/v1/recognizeText"
 
@@ -54,16 +45,25 @@ def main(image_path, iam_token_dir):
 
     if response.status_code == 200:
          data_json = response.json()
-         file_name = "C:\\DEV_python\\YandexVision\\result_files_json\\file.json"
-         with open(file_name, "w", encoding="utf-8") as json_file:
+         print(data_json)
+         # Define the directory path
+         directory = settings.privacy.result_json_folder
+
+         # Create the directory if it doesn't exist
+         if not os.path.exists(directory):
+             os.makedirs(directory)
+
+         # Define the file path
+         file_path = directory + "file.json"
+
+         with open(file_path, "w", encoding="utf-8") as json_file:
             json.dump(data_json, json_file, ensure_ascii=False, indent=4)
-        # print(response.json())
     else:
         print("Ошибка", response.status_code)
         
 
 if __name__ == '__main__':
     iam_token_dir = os.path.join(os.getcwd(), "iam_token")
-    image_path = "C:\\DEV_python\\YandexVision\\input_image\\1.jpg"
+    image_path = "C:\\RPA\\python\\YandexVision\\incoming\\photo_2024-04-16_10-50-14.jpg"
     main(image_path, iam_token_dir)
-    # main(input_image_file)
+    # print(iam_token_dir)
